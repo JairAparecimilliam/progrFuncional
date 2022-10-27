8629668df063bcb64c3644b9956a1e6b70ac3631
Jair Correa <jrabc1991@gmail.com>
2022-10-26 23:36:39 -0300

    Função anonima e função LAMBDA UPDATE

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,11 +15,9 @@ public class Program {
 		list.add(new Product("Notebook", 1200.0));
 		list.add(new Product("Tablet", 450.0));
 
-		// (p1, p2) -> levando a implementação da função anonima a seguir
-		Comparator<Product> comp = (p1, p2) -> {
-			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-		};
-
+		//comp vai ser uma função que recebe dois argumentos p1 e p2 e a implementação da função depois da arrow 
+		Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+		
 		list.sort(comp);
 
 		for (Product p : list) {
